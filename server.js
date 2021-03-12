acfaf6ac572fe8c3ef23840ee331d650457de023
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-12 16:40:09 +0200

    post user name
    when adding user, response is username and id.

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const User = require('./models/user')
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
+app.use(bodyParser());
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/api/exercise/new-user', (req, res) => {
+  console.log(req.body);
+    const { body } = req;
+    const user = new User({
+      username: body.username,
+    })
+    user.save()
+    .then(result => {
+      res.status(200).json(result);
+    })
 });
 
 
@@ -14,5 +30,5 @@ app.get('/', (req, res) => {
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+    console.log('Your app is listening on port ' + listener.address().port)
+});

d2a0eafe2c556aaa5b7c3012ec2ccdfb8bac8976
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-13 17:10:16 +0200

    add exercise

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const User = require('./models/user');
+const { User } = require('./models/user');
+const { Exercise } = require('./models/user');
 
 app.use(cors());
 app.use(express.static('public'));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
@@ -22,14 +23,48 @@ app.post('/api/exercise/new-user', (req, res) => {
     .then(result => {
       const adjustedResult = {
         username: result.username,
-        id: result.id,
+        _id: result.id,
       }
       res.status(200).json(adjustedResult);
     })
 });
 
+app.post('/api/exercise/add', (req, res) => {
+  const { body } = req;
+  const id = body.userId;
+  const exercise = new Exercise({
+    description: body.description,
+    duration: Number(body.duration),
+    date: body.date,
+  })
+  if(exercise.date === ''){
+    exercise.date = 
+    new Date()
+    .toISOString()
+    .slice(0, 10);
+  }
+  User.findByIdAndUpdate(
+      id,
+      {$push: { log: exercise }},
+      { new: true }
+    )
+  .then(user => {
+    const responseObj = {
+      _id: user._id,
+      username: user.username,
+      date: new Date(exercise.date).toDateString(),
+      description: exercise.description,
+      duration: exercise.duration,
+    }
+    res.status(200).json(responseObj);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
+
 app.get('/api/exercise/users', (req, res) => {
-    User.find()
+    User.find({}).select(['-log', '-__v'])
     .then(users => {
         console.log(users);
         res.status(200).json(users);
